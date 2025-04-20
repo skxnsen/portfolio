8e12c5f25d495b40ad4ccb2eeb1278513c258228
emkeyen <emkeyen96@gmail.com>
2025-04-20 21:06:44 +0200

    fix github actions pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,30 +3,30 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   timeout: 30 * 1000,
-  expect: {
-    timeout: 5000,
-  },
+  expect: { timeout: 5000 },
   fullyParallel: true,
-  forbidOnly: !!process.env['CI'],
-  retries: process.env['CI'] ? 2 : 0,
-  workers: process.env['CI'] ? 1 : undefined,
-  reporter: [['html', { outputFolder: 'tests/reports', open: 'never' }]],
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
   use: {
-    baseURL: process.env['BASE_URL'] || 'http://127.0.0.1:4200',
+    baseURL: 'http://localhost:4200',
+    trace: 'on-first-retry',
   },
   webServer: {
     command: 'npx http-server ./dist/angular-portfolio-app -p 4200',
-    url: process.env['BASE_URL'] || 'http://127.0.0.1:4200',
-    reuseExistingServer: !process.env['CI'],
-    timeout: 60 * 1000, // 👈 prevents timeout error
+    url: 'http://localhost:4200',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+    stderr: 'pipe',
   },
   projects: [
     {
-      name: 'Chromium',
+      name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
     {
-      name: 'Firefox',
+      name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
   ],
