8761eb6f53f74e016e57bed24324f340caf0d172
emkeyen <emkeyen96@gmail.com>
2025-05-02 14:57:27 +0200

    update home page component tests

--- tests/pom/home_page.ts
+++ tests/pom/home_page.ts
@@ -4,11 +4,18 @@ export class HomePage {
   readonly page: Page;
   readonly dynamicText: Locator;
   readonly portfolioButton: Locator;
+  readonly authorName: Locator;
+  readonly homeTitle: Locator;
+  readonly homeSubtitle: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.dynamicText = page.locator('#dynamic-text');
     this.portfolioButton = page.locator('button >> text=Portfolio');
+    this.authorName = page.locator('.author-name');
+    this.homeTitle = page.locator('.home-title');
+    this.homeSubtitle = page.locator('.home-subtitle');
+    
   }
 
   async navigateMain() {
