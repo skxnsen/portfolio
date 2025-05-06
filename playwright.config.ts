2050b0c80e2c7628b64bad20a84063184b584a44
emkeyen <emkeyen96@gmail.com>
2025-05-07 00:15:25 +0200

    upd playwright config to cover local & prod envs

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,16 +10,18 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
   use: {
+    // Use the BASE_URL environment variable (e.g., https://skansen.netlify.app/)
     baseURL: process.env.BASE_URL || 'http://localhost:4200',
     trace: 'on-first-retry',
   },
   webServer: {
+    // Command to run the local server only if no BASE_URL is provided
     command: 'npx serve -s ./dist/angular-portfolio-app/browser -l 4200',
-    url: process.env.BASE_URL || 'http://localhost:4200',
-    reuseExistingServer: !process.env.CI,
+    url: 'http://localhost:4200', // Default local URL
+    reuseExistingServer: !process.env.CI && !process.env.BASE_URL, // Avoid starting the server when BASE_URL is set
     timeout: 180 * 1000, // 3 minutes
     stdout: 'pipe',
-    stderr: 'pipe'
+    stderr: 'pipe',
   },
   projects: [
     {
